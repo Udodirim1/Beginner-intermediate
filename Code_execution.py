4ec46b8e6ae90539a23973d7cca5684152af8682
Udodirim1 <5nowsk81@solent.ac.uk>
2022-05-14 06:00:15 +0100

    updating

--- Code_execution.py
+++ Code_execution.py
@@ -1,4 +1,62 @@
-print('Hello, my name is Udodirim Nwosu')
-print('o----')
-print(' ||||')
-print('*' * 10)
+# FIRST CODE EXECUTION
+# print('Hello, my name is udodirim nwosu')
+# print('o----')
+# print(' ||||')
+# print('*' * 10)
+
+#-----------------------------------------------------------
+
+#VARIABLES
+# these are called simple values
+
+# price = 10   #integer
+# rating = 4.9   #float
+# name = 'Mosh'   #string
+# is_published = False   #boolean value
+# print(price)
+
+#------------------------------------------------------------
+
+# GETTING INPUT
+# name = input('What is your name? ')
+# print('Hi ' + name)
+
+#------------------------------------------------------------
+
+# TYPE CONVERSIONS
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# age = 2019 - int(birth_year)
+# print(type(age))
+# print(age)
+
+#------------------------------------------------------------
+
+#STRINGS
+# course = "Python's course for beginners"
+#
+# #square bracket syntax
+# print(course[0])
+# print(course[-1])
+# print(course[0:])
+# print(course[0:6])
+# print(course[:6])
+# print(course[:])
+# print(course[1:-1])
+
+#------------------------------------------------------------
+
+#FORMATTED STRINGS
+# first_name = 'John'
+# last_name = 'Smith'
+# message = first_name + ' [' + last_name + '] is a coder'
+# msg = f'{first_name} [{last_name}] is a coder'
+# print(msg)
+
+#------------------------------------------------------------
+
+#STRING METHODS
+course = 'Python for Beginners'
+print(len(course))
+print(course.upper())
+print(course.lower())
---
+++ EXERCISES.py
@@ -0,0 +1,32 @@
+# FIRST EXERCISE
+# Check in a patient named John Smith, 20years, new patient.
+# name, age, patient
+
+# name = 'John Smith'
+# age = 20
+# new_patient = True
+
+#-------------------------------------------------------------
+
+# SECOND EXERCISE
+# Ask two questions: person's name & favourite color.
+# Then, print a message like 'Mosh likes blue'
+
+# name = input('What is your name? ')
+# color = input('What is your favorite color? ')
+# print(name + ' likes ' + color)
+
+#-------------------------------------------------------------
+
+# THIRD EXERCISE
+# Ask a user their weight (in pounds), convert to kilograms
+# print on the terminal.
+
+# weight_lbs = input('What do weigh in pounds? ')
+# weight_kg = int(weight_lbs) * 0.45
+# print(weight_kg)
+
+#-------------------------------------------------------------
+
+#FOURTH EXERCISE
+#
